0ce0c120fd47f7b66157b3b337674f038fa72fc2
VictosCream - Ashuri <snowliream@gmail.com>
2025-07-28 23:33:48 +0800

    Update index.php

--- HTDB/index.php
+++ HTDB/index.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Hotel Management System - Login Selection</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
 
+<header>Hotel Management and Booking System</header>
+
+<div class="selection-container">
+    <h1>Welcome</h1>
+    <div class="login-buttons">
+        <a href="user_selection.php">Login/Sign Up as User</a>
+        <a href="admin_selection.php">Login/Sign Up as Admin</a>
+    </div>
+</div>
+
+</body>
+</html>
